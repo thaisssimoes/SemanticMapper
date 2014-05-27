a9141792d1b35bf310892642df029067cdc76017
felipeleao <felipe@fleao.com.br>
2014-05-27 01:41:06 -0300

    Added user confirmation before schema drop
    Added a step where the user is asked if he really wishes to run the
    application aware that the DB schema will be dropped and all data lost.

--- src/main/java/br/uniriotec/ppgi/mapping/view/Main.java
+++ src/main/java/br/uniriotec/ppgi/mapping/view/Main.java
@@ -1,22 +1,48 @@
 package br.uniriotec.ppgi.mapping.view;
 
 import java.io.IOException;
-
-import org.apache.log4j.Logger;
+import java.util.Scanner;
 
 import br.uniriotec.ppgi.mapping.controller.run.Runner;
 
 public class Main {
-	private static Logger logger = Logger.getLogger(Main.class);
-	
+	private static Scanner scan = new Scanner(System.in);
+    private static String userInput = null;
+    
 	public static void main(String[] args) throws IOException {
 		
-		logger.info("*** Starting Supersense Mapper ***");
+		/*
+		 * Warns the user that the Database schema will be dropped and
+		 * all current data will be lost. Aske if He/She wishes to proceed.
+		 */
+		System.out.println("===============================================");
+		System.out.println("==    Supersense to Semantic Type Mapper     ==");
+		System.out.println("===============================================");
+		System.out.println("**ATENTION** If currently there is a database schema created and populated IT WILL BE DROPPED! ");
+		System.out.println("Make sure to backup all important data before running the application.");
+		System.out.print("Do you wish to continue running? [Y/N]: ");
+		userInput = scan.nextLine();
 		
-		Runner runner = new Runner();
-		runner.run();
+		/* ****************************************
+		 * 		If YES, run the application 
+		 * ****************************************/
+		if (userInput.equalsIgnoreCase("y") || userInput.equalsIgnoreCase("yes")){
+			
+			System.out.println("*** Starting Supersense Mapper ***");
+			Runner runner = new Runner();
+			runner.run();
+			System.out.println("*** Finishing Supersense Mapper ***");
+			
+			
+		/* ****************************************
+		 * 				If NO, abort!
+		 * ****************************************/
+		}else{
+			System.out.println("\n*** Application aborted by user! ***");
+		}
+        
 		
-		logger.info("*** Finishing Supersense Mapper ***");
+		
 	}
 
 }
