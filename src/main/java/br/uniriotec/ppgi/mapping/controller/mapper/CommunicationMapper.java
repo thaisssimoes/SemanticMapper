cf31211006c5f82efd815e785db6fa9e48d4c54d
felipeleao <felipe@fleao.com.br>
2014-05-27 23:13:24 -0300

    Added javadoc to CommunicationMapper class

--- src/main/java/br/uniriotec/ppgi/mapping/controller/mapper/CommunicationMapper.java
+++ src/main/java/br/uniriotec/ppgi/mapping/controller/mapper/CommunicationMapper.java
@@ -2,8 +2,21 @@ package br.uniriotec.ppgi.mapping.controller.mapper;
 
 import br.uniriotec.ppgi.mapping.model.MySynset;
 
+/**
+ * Maps Wordnet Synsets associated to the noun.communication supersense
+ * to other semantical classes.
+ * 
+ * @author felipe
+ *
+ */
 public class CommunicationMapper {
 	
+	/**
+	 * Maps noun.communication synsets to the corresponding
+	 * semantic type described by Dixon.
+	 * 
+	 * @param synset
+	 */
 	public static void mapToSemType(MySynset synset){
 		System.out.println("found "+synset.getSupersense().getName());
 	}
